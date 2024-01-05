f0116885fe1d4af6e361fa516d99ad69c4775f7a
Nikolask2299 <124400530+Nikolask2299@users.noreply.github.com>
2024-01-05 23:20:25 +0400

    Add files via upload

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -11,15 +11,15 @@ import (
 
 const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
 	log := setupLogger(cfg.Env)
-	log.Info( "starting server aplication with environment",
+	log.Info("starting server aplication with environment",
 		slog.Any("cfg", cfg),
 	)
 
@@ -28,26 +28,34 @@ func main() {
 	go application.GRPCSrv.MustRun()
 
 	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)	
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	
+	sigerr := <- stop
+
+	log.Info("shutting down server aplication with environment", slog.String("sigerr", sigerr.String()))
+
+	application.GRPCSrv.Stop()
+
+	log.Info("application stopped successfully")
 }
+
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
-
 	switch env {
-		case envLocal:
-			log = slog.New(
-				slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}), 
-			)
-		case envDev:
-			log = slog.New(
-				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-			)
-		case envProd:
-			log = slog.New(
-				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-				)
-		}
-		return log
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
--- sso/interal/grpc/auth/server.go
+++ sso/interal/grpc/auth/server.go
@@ -3,21 +3,54 @@ package auth
 import (
 	"context"
 	ssov1 "protos/gen/go/sso"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const (
+	emptyId = 0
+	emptyEmPw = ""
+)
+
+
+type Auth interface {
+	Login(ctx context.Context, email string, password string, appId int) (token string, err error)
+	RegisterNewUser(ctx context.Context, email string, password string) (userID int, err error)
+	IsAdmin(ctx context.Context, userID int) (isAdmin bool, err error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
-
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server){
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth){
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
 	
-	return &ssov1.LoginResponse{Token: req.GetEmail() + " Fell_Good_Inc",}, nil
+	if req.GetEmail() == emptyEmPw {
+		return nil, status.Error(codes.InvalidArgument, "Email cannot be empty")
+	}
+
+	if req.GetPassword() == emptyEmPw {
+        return nil, status.Error(codes.InvalidArgument, "Password cannot be empty")
+    }
+
+	if req.GetAppId() == emptyId {
+		return nil, status.Error(codes.InvalidArgument, "AppId cannot be empty")
+	}
+
+ 	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId())) 
+	if err != nil {
+		//TODO: log error and return error to client
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &ssov1.LoginResponse{Token: token}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error){
@@ -26,4 +59,23 @@ func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*
 
 func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error){
 	panic("implement me")
+}
+
+
+
+func isValidationRequest(req *ssov1.LoginRequest) error {
+
+	if req.GetEmail() == emptyEmPw {
+		return status.Error(codes.InvalidArgument, "Email cannot be empty")
+	}
+
+	if req.GetPassword() == emptyEmPw {
+        return status.Error(codes.InvalidArgument, "Password cannot be empty")
+    }
+
+	if req.GetAppId() == emptyId {
+		return status.Error(codes.InvalidArgument, "AppId cannot be empty")
+	}	
+	
+	return nil
 }
